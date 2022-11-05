11892a8fc521c67d668b2df79e0b0559354d8164
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2022-11-06 00:03:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ sheetTitleEl.value = "Untitled Spreadsheet";
 sheetTitleEl.addEventListener('change',(e)=>{
     
     document.title = `${sheetTitleEl.value} - Sheets`;
-})
+});
+
+const row = 100;
+const col = 26;
+
+let $rowContainer = document.querySelector('.row-container');
+let $colContainer = document.querySelector('.col-container');
+
+for(let i = 1; i<=row ;i++){
+    let $rowEl = document.createElement('div');
+    $rowEl.classList.add(`row-count`);
+    $rowEl.textContent = i;
+    $rowContainer.appendChild($rowEl);
+    
+}
+
+for(let i = 0;i<26;i++){
+    let $colEl = document.createElement('div');
+    $colEl.classList.add('col-count');
+    $colEl.textContent = String.fromCharCode(65+ i);
+    $colContainer.appendChild($colEl);
+}
